d0bff87cb74c21dd131dda22750ac66eb7aa33af
Harisawa <harisaw2@illinois.edu>
2017-10-21 16:26:27 -0500

    PIC flags added

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -10,6 +10,7 @@
 /* Initialized so that all IRQs were initially masked */
 uint8_t master_mask = 0xFF; /* IRQs 0-7  */
 uint8_t slave_mask = 0xFF;  /* IRQs 8-15 */
+uint32_t flags;
 //spinlock_t irq_lock;
 
 
@@ -29,14 +30,8 @@ uint8_t slave_mask = 0xFF;  /* IRQs 8-15 */
 void i8259_init(void) {
 	
 	/* masking interrupts on the processor and acquiring a lock */
-	//unsigned long flags;
-	//spin_lock_irqsave(irq_lock, flags);
-	
-	/* masks all interrupts (on both PICs) */
-	/* active low */
-	outb(MASTER_8259_DATA, 0xFF);
-	outb(SLAVE_8259_DATA, 0xFF);
-	
+
+	cli_and_save(flags); 	
 	/* executes the initialization sequence */
 	/* for master PIC */
 	outb(MASTER_8259_PORT, ICW1);
@@ -50,17 +45,20 @@ void i8259_init(void) {
 	outb(SLAVE_8259_DATA, ICW4);
 	
 	/* restores the mask settings */
-	outb(MASTER_8259_DATA, master_mask);
-	outb(SLAVE_8259_DATA, slave_mask);
+	//outb(MASTER_8259_DATA, master_mask);
+	//outb(SLAVE_8259_DATA, slave_mask);
 	
-	/* releases the lock, and restores the IF flag */
-	//spin_unlock_irqrestore(irq_lock, flags);
-	return;
+	outb(MASTER_8259_DATA, 0xFF);
+	outb(SLAVE_8259_DATA, 0xFF);
+	restore_flags(flags);
+	
+	enable_irq(2); // irq 2 is slave pic
 }
 
 /* NODENS HAVEN'T COMMENTED YET */
 /* Enable (unmask) the specified IRQ */
 void enable_irq(uint32_t irq_num) {
+	cli_and_save(flags); 
 	uint8_t interrupt_unmask;
 	if(irq_num >= 0 && irq_num <= 7) {
 		interrupt_unmask = ~(0x01 << irq_num);
@@ -68,16 +66,18 @@ void enable_irq(uint32_t irq_num) {
 		outb(MASTER_8259_DATA, master_mask);
 	}
 	else if(irq_num >= 8 && irq_num <= 15) {
+		//enable_irq(2); // irq 2 is slave pic
 		interrupt_unmask = ~(0x01 << (irq_num - 8));
 		slave_mask = slave_mask & interrupt_unmask;
 		outb(SLAVE_8259_DATA, slave_mask);
 	}
-	return;
+	restore_flags(flags);
 }
 
 /* NODENS HAVEN'T COMMENTED YET */
 /* Disable (mask) the specified IRQ */
 void disable_irq(uint32_t irq_num) {
+	cli_and_save(flags); 
 	uint8_t interrupt_mask;
 	if(irq_num >= 0 && irq_num <= 7) {
 		interrupt_mask = (0x01 << irq_num);
@@ -89,12 +89,13 @@ void disable_irq(uint32_t irq_num) {
 		slave_mask = slave_mask | interrupt_mask;
 		outb(SLAVE_8259_DATA, slave_mask);
 	}
-	return;	
+	restore_flags(flags);
 }
 
 /* NODENS HAVEN'T COMMENTED YET */
 /* Send end-of-interrupt signal for the specified IRQ */
 void send_eoi(uint32_t irq_num) {
+	cli_and_save(flags); 
 	if(irq_num >= 0 && irq_num <= 7) {
 		outb(MASTER_8259_PORT, (EOI | irq_num));
 	}
@@ -102,5 +103,5 @@ void send_eoi(uint32_t irq_num) {
 		outb(MASTER_8259_PORT, (EOI | 2));
 		outb(SLAVE_8259_PORT, (EOI | (irq_num - 8)));
 	}
-	return;
+	restore_flags(flags);
 }
