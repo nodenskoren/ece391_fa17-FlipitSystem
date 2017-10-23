16b3f25730479c9896de243d6e46da40dcf4bf90
harisaw2 <harisaw2@illinois.edu>
2017-10-22 22:55:07 -0500

    Added comment in RTC

--- student-distrib/RTC_driver.h
+++ student-distrib/RTC_driver.h
@@ -1,5 +1,8 @@
 #include "lib.h"
 #include "i8259.h"
 
+/* Initialization function for RTC driver */
 extern void initialize_RTC_driver();
+
+/* Interrupt handler for RTC driver */
 void RTC_interrupt_handler();
