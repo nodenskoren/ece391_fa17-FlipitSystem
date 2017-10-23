ef3932d4f53b3462535a315e327131be0b68687f
harisaw2 <harisaw2@illinois.edu>
2017-10-22 22:56:27 -0500

    Added comment in IDT

--- student-distrib/initialize_idt.h
+++ student-distrib/initialize_idt.h
@@ -2,8 +2,10 @@
 #include "i8259.h"   
 #include "RTC_driver.h"
 
+/* Initialization function for IDT */
 extern void initialize_idt();
 
+/* Exception handlers for first 20 exceptions defined by Intel */
 void exception_0 ();
 void exception_1 ();
 void exception_2 ();
@@ -24,5 +26,7 @@ void exception_16 ();
 void exception_17 ();
 void exception_18 ();
 void exception_19 ();
+
+/* Handler for system call */
 void syscall_handler();
 
