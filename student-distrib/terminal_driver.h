dd9aa0322413dc9f54720f81eed7dd20e4c6159b
Zurcher <nzurche2@illinois.edu>
2017-10-29 15:31:11 -0500

    more comments

--- student-distrib/terminal_driver.h
+++ student-distrib/terminal_driver.h
@@ -3,18 +3,21 @@
 
 #include "lib.h"
 
+
+/*system call opens file*/
 extern int32_t terminal_open();
-
+/*system call to close file*/
 extern int32_t terminal_close();
-
+/*system call reads last entered command*/
 extern int32_t terminal_read(char * buf, int32_t nbytes);
-
+/*system call writes to terminal*/
 extern int32_t terminal_write(const char* buf,int32_t nbytes);
 
+/*adds ascii character from keyboard to keyboard buffer*/
 extern void add_to_buffer(char c);
-
+/*clears keyboard buffer*/
 extern void clear_buffer();
-
+/*coppies keyboard buffer to command buffer*/
 extern void copy_command_buffer();
 
 #endif
