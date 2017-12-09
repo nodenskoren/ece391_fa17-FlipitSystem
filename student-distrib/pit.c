bbc0b96efb121dfff3d7ec0fe77a66d29da3f00d
harisaw2 <harisaw2@illinois.edu>
2017-12-08 18:12:13 -0600

    Update pit.c

--- student-distrib/pit.c
+++ student-distrib/pit.c
@@ -70,6 +70,65 @@ void pit_handler(){
 	
 }
 
+/*
+ * pit_freq_change
+ *   DESCRIPTION: Change the frequency of pit
+ *   INPUTS: uint32_t nFrequence
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: none
+ */
+
+void pit_freq_change(uint32_t nFrequence){
+	
+ 	uint32_t Div = 1193180 / nFrequence;
+ 	outb((uint8_t) (Div), 0x42);
+ 	outb((uint8_t) (Div >> 8), 0x42);	
+	
+}
+
+/*
+ * play_sound
+ *   DESCRIPTION: Play beep sound 
+ *   INPUTS: uint32_t nFrequence
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: none
+ */
+
+ void play_sound(uint32_t nFrequence) {
+ 	uint32_t Div;
+ 	uint8_t tmp;
+ 
+        //Set the PIT to the desired frequency
+ 	Div = 1193180 / nFrequence;
+ 	outb(0xb6, 0x43);
+ 	outb((uint8_t) (Div), 0x42);
+ 	outb((uint8_t) (Div >> 8), 0x42);
+ 
+        //And play the sound using the PC speaker
+ 	tmp = inb(0x61);
+  	if (tmp != (tmp | 3)) {
+ 		outb( tmp | 3, 0x61);
+ 	}
+ }
+ 
+/*
+ * nosound
+ *   DESCRIPTION: Make the sound shut up
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: none
+ */
+ void nosound() {
+ 	uint8_t tmp = inb(0x61) & 0xFC;
+ 
+ 	outb(tmp, 0x61);
+ }
+ 
+
+
 
 
 
